154ca652b6149342a5e781e4180f1ba91cac42f5
Daniel Capeans <dcapeans@gmail.com>
2021-06-30 17:35:45 -0300

    added test for product response body

--- tests/products.test.js
+++ tests/products.test.js
@@ -7,6 +7,18 @@ describe("GET /products", () => {
     it("returns status 200 for success", async () => {
         const result = await supertest(app).get("/products");
         expect(result.status).toEqual(200);
+        
+        expect(result.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              category: expect.any(String),
+              id: expect.any(Number),
+              image: expect.any(String),
+              name: expect.any(String),
+              price: expect.any(String)
+            })
+          ])
+        )
       });
 })
 
