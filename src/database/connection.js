c1a01f77e3613b0c8672953ccbaf59b9d62f5af4
Daniel Capeans <dcapeans@gmail.com>
2021-07-01 12:22:58 -0300

    Preparation for Heroku deploy

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,12 +2,19 @@ import pg from "pg";
 
 const { Pool } = pg;
 
+// const connection = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_DATABASE,
+// });
+
 const connection = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default connection;
